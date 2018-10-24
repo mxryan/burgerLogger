442eff15d7f360ccb49c572b8883a5f9ebd97716
M Ryan <mxryan@live.com>
2018-10-23 22:38:06 -0400

    basic logic completed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,4 @@
+const burger = require("../models/burger");
 module.exports = (app) => {
   app.get("/", (req,res)=>{
     burger.getAll((data)=>{
@@ -6,4 +7,21 @@ module.exports = (app) => {
       res.render("index", hbsObj);
     });
   });
+  app.post("/", (req, res)=>{
+    burger.update({devoured: 1}, "ID="+req.body.id, (err, data)=>{
+      if (err) return console.log(err);
+      console.log(res);
+      res.end();
+      
+    });
+  });
+  app.post("/new", (req, res)=>{
+    console.log(req.body);
+    burger.create(["burger_name"], [req.body.name],(err,data)=>{
+      if (err) return console.log(err);
+      console.log(data);
+      res.redirect("/");
+    });
+  });
+  
 }
--- server.js
+++ server.js
@@ -1,46 +1,23 @@
 //todo: 
-// -modulize routes
 // -add css
 // -front end js to make visualization match db status
-// -separate front end javascript
+// -switch from jquery to fetch?
 // -get post responses working?
 const express = require("express");
 const hbs = require("express-handlebars");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const burger = require("./models/burger");
+
 app.use(express.static("public")); 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.engine("handlebars",hbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-app.get("/", (req,res)=>{
-  burger.getAll((data)=>{
-    const hbsObj = {burgers: data};
-    console.log(data);
-    res.render("index", hbsObj);
-  });
-});
 
-app.post("/", (req, res)=>{
-  burger.update({devoured: 1}, "ID="+req.body.id, (err, data)=>{
-    if (err) return console.log(err);
-    console.log(res);
-    res.end();
-    
-  });
-});
+setRoutes = require("./controllers/burgers_controller")
 
-app.post("/new", (req, res)=>{
-  console.log(req.body);
-  burger.create(["burger_name"], [req.body.name],(err,data)=>{
-    if (err) return console.log(err);
-    console.log(data);
-    res.redirect("/");
-  });
-  
-})
+setRoutes(app);
 
 app.listen(PORT, ()=>{
   console.log("server on");
